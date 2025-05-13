c759a5828611a59dc2486ec237817df1a82a2d8c
Almar Klein <almar@almarklein.org>
2025-05-13 11:00:20 +0200

    Add examples for simulations

---
+++ examples/simulation3.py
@@ -0,0 +1,11 @@
+"""
+Running a simulation, part 3
+----------------------------
+
+In this example, we run a simulation (Conway's Game of Life),
+using the animation events.
+
+This allows simulations that must keep up with the real world.
+"""
+
+# TODO: implement animation events
