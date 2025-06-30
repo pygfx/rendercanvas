e0331e2820ca1e70220809be889433d657254bb3
Almar Klein <almar@almarklein.org>
2025-06-30 15:03:09 +0200

    disallow <1

--- rendercanvas/_scheduler.py
+++ rendercanvas/_scheduler.py
@@ -95,9 +95,9 @@ class Scheduler:
 
         if max_fps is not None:
             # For users setting max_fps=-1 to try to disable the fps cap, as was the way to go in wgpu.gui
-            if max_fps < 0:
+            if max_fps < 1:
                 raise ValueError(
-                    "max_fps should be a positive number. Use update_mode='fastest' to disable the fps cap. Also see ``vsync=False`` to unlock higher frame rates."
+                    "max_fps should be a >= 1. Use update_mode='fastest' to disable the fps cap. Also see ``vsync=False`` to unlock higher frame rates."
                 )
             self._max_fps = max(1, float(max_fps))
 
