b06a8a180f20daa9fff2b61901f088a62efedafd
Almar Klein <almar@almarklein.org>
2025-11-01 22:41:12 +0100

    use nearest neighbour

--- rendercanvas/pyodide.py
+++ rendercanvas/pyodide.py
@@ -495,9 +495,11 @@ class PyodideRenderCanvas(BaseRenderCanvas):
             # This effectively uploads the image to a GPU texture (represented by the offscreen canvas).
             self._offscreen_canvas.getContext("2d").putImageData(image_data, 0, 0)
             # Then we draw the offscreen texture into the real texture, scaling is applied.
-            self._canvas_element.getContext("2d").drawImage(
-                self._offscreen_canvas, 0, 0, cw, ch
-            )
+            # Do we want a smooth image or nearest-neighbour? Depends on the situation.
+            # We should decide what we want backends to do, and maybe have a way for users to chose.
+            ctx = self._canvas_element.getContext("2d")
+            ctx.imageSmoothingEnabled = False
+            ctx.drawImage(self._offscreen_canvas, 0, 0, cw, ch)
 
     def _rc_set_logical_size(self, width: float, height: float):
         self._canvas_element.style.width = f"{width}px"
