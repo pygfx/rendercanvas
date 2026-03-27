53526f8685acb5cedeba9027cc946ab06e8c8064
Almar Klein <almar@almarklein.org>
2026-03-27 10:41:58 +0100

    Fix that Qt update() and wx Refresh did not cause a new draw

--- rendercanvas/wx.py
+++ rendercanvas/wx.py
@@ -240,7 +240,7 @@ class WxRenderWidget(BaseRenderCanvas, wx.Window):
 
     def _on_resize_done(self, *args):
         self._draw_lock = False
-        self.Refresh()
+        self.request_draw()
 
     def on_paint(self, event):
         dc = wx.PaintDC(self)
@@ -251,6 +251,10 @@ class WxRenderWidget(BaseRenderCanvas, wx.Window):
         else:
             event.Skip()
         del dc
+
+    def Refresh(self):  # noqa: N802
+        # Bypass wx mechanics and request a draw, which will eventually call the native Refresh()
+        self.request_draw()
 
     def _get_surface_ids(self):
         if sys.platform.startswith("win") or sys.platform.startswith("darwin"):
@@ -322,12 +326,12 @@ class WxRenderWidget(BaseRenderCanvas, wx.Window):
         if self._draw_lock:
             return
         try:
-            self.Refresh()
+            wx.Window.Refresh(self)
         except Exception:
             pass  # avoid errors when window no longer lives
 
     def _rc_force_paint(self):
-        self.Refresh()
+        wx.Window.Refresh(self)
         self.Update()
         if sys.platform == "darwin":
             wx.Yield()
@@ -580,6 +584,10 @@ class WxRenderCanvas(WrapperRenderCanvas, wx.Frame):
 
         self.Show()
         self._final_canvas_init()
+
+    def Refresh(self):  # noqa: N802
+        self._subwidget.request_draw()
+        super().Refresh()
 
 
 # Make available under a name that is the same for all gui backends
