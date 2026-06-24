ed802cfa5527a7583ce5ca06505047e4c62c8154
Almar Klein <almar@almarklein.org>
2026-06-24 14:55:22 +0200

    remove temp test code from example

--- examples/cube_auto.py
+++ examples/cube_auto.py
@@ -7,7 +7,7 @@ Run a wgpu example on an automatically selected backend.
 
 # run_example = true
 
-from rendercanvas.http import RenderCanvas, loop
+from rendercanvas.auto import RenderCanvas, loop
 from rendercanvas.utils.cube import setup_drawing_sync
 
 canvas = RenderCanvas(
@@ -15,13 +15,6 @@ canvas = RenderCanvas(
 )
 draw_frame = setup_drawing_sync(canvas)
 canvas.request_draw(draw_frame)
-
-
-@canvas.add_event_handler("key_down")
-def process_event(event):
-    if event["key"] == " ":
-        # print(canvas.get_stats())
-        canvas.set_logical_size(300, 300)
 
 
 if __name__ == "__main__":
