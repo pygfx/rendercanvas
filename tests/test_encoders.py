76997ec6674bd6fd0f4b1f245013dc16e10f4950
Almar Klein <almar@almarklein.org>
2026-07-06 12:39:41 +0200

    fix encoder test

--- tests/test_encoders.py
+++ tests/test_encoders.py
@@ -9,10 +9,8 @@ import numpy as np
 
 def get_random_im(*shape):
     """Get a random image."""
-    im = np.random.randint(0, 100, shape).astype(np.uint8)
-    im //= 4
-    im *= 4
-    return im
+    # With 2 possible values, you get enough entropy vs structure for png to scale compression
+    return np.random.randint(0, 2, shape).astype(np.uint8) * 100
 
 
 def test_encode_array():
@@ -79,7 +77,8 @@ def test_encode_jpeg():
 def test_encode_png():
     """Tests for encode_jpeg function."""
 
-    _perform_checks(encode_png, 3, 9)
+    # level 9 tends to try too hard and actually produce larger blobs
+    _perform_checks(encode_png, 3, 8)
     _perform_error_checks(encode_png)
 
 
